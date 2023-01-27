98503f2d01b861e19a2a9572a92890c8ce6ddd3c
hyper-neutrino <hyperneutrino15@gmail.com>
2023-01-26 20:50:22 -0500

    set vite preview port to 5173

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import type { UserConfig } from "vite";
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	server: { fs: { allow: ['./static'] } }
+    plugins: [sveltekit()],
+    server: { fs: { allow: ["./static"] } },
+    preview: { port: 5173 },
 };
 
 export default config;
